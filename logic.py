9ed67dc415c7f3e610d173ac22526d77fb6bc996
Gracie <battlesbrown@gmail.com>
2024-10-06 21:30:17 -0500

    Add character class

--- logic.py
+++ logic.py
@@ -5,4 +5,34 @@ Logic
 By Ann-MarieDev
 ---------------
 """
+#---------------CLASSES---------------
+
+#Character Class
+class character:
+    def __init__(self, name, age, health, power, hair_color, skin_color, eye_color):
+        self.name = name
+        self.age = age
+        self.health = health
+        self.power = power
+        self.hair_color = hair_color
+        self.skin_color = skin_color
+        self.eye_color = eye_color
+
+        
+#---------------CHARACTERS---------------
+#--Main Characters--
+#Nala
+main_char = character("Nala", 16, 100, 3, "orange", "tan", "green")
+#Orlando
+main_char_two = character("Orlando", 17, 100, 5, "brown", "tan", "brown")
+
+#--Enemies--
+#Alice
+enemy_one = character("Alice", 17, 100, 5, "blonde", "pale", "blue")
+
+#--Side Characters/NPC's--
+
+
+#---------------VARIABLES---------------
+
 
