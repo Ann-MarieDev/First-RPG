ee1ddc74fa7dd32bd25814db0c156454bae55ebb
Gracie Hutchins <battlesbrown@gmail.com>
2024-10-07 09:43:10 -0500

    Add weapons and weapon class variables

--- logic.py
+++ logic.py
@@ -5,7 +5,7 @@ Logic
 By Ann-MarieDev
 ---------------
 """
-#Test if logic.py is working on main.py
+#---------------Test if logic.py is working on main.py---------------
 def test_logic():
     print("logic.py is working")
     
@@ -24,10 +24,11 @@ class character:
         
 #Weapon Class
 class weapon:
-    def __init__(self, name, damage, weight):
+    def __init__(self, name, damage, weight, weapon_class):
         self.name = name
         self.damage = damage
         self.weight = weight
+        self.weapon_class = weapon_class
         
 #Clothes/Armor
 class weapon:
@@ -37,9 +38,7 @@ class weapon:
         self.weight = weight
         self.type = type
         self.color = color
-                
 
-        
 #---------------CHARACTERS---------------
 #--Main Characters--
 #Nala
@@ -52,12 +51,47 @@ main_char_two = character("Orlando", 17, 100, 5, "brown", "tan", "brown")
 enemy_one = character("Alice", 17, 100, 5, "blonde", "pale", "blue")
 
 #--Side Characters/NPC's--
+#---------------VARIABLES---------------
+"""
+Weapon classes:
+- Worst
+- Bad
+- Good
+- Best
+- Legendary
+"""
+worst = "WEAPON CLASS: Worst"
+bad = "WEAPON CLASS: Bad"
+good = "WEAPON CLASS: Good"
+best = "WEAPON CLASS: Best"
+legendary = "WEAPON CLASS: Legendary"
 
 #---------------WEAPONS---------------
-#Starter items
+#Copy n paste: weapon("",,,),
+#weapon(Name,  Damage, Weight,  Weapon Class),
+weapons = [
+    #---------------Worst---------------
+    weapon("Baloon Sword", 0.5, 0.2, worst),
+    weapon("Plastic Sword", 1, 0.5, worst),
+    weapon("Clown Hammer", 0.5, 1, worst),
+    weapon("Broken Pencil", 0, 0.1, worst),
+    #---------------Bad---------------
+    weapon("Sling Shot", 0, 4, bad),
+    weapon("Piñata Stick", 2, 2, bad),
+    weapon("Wooden Sword", 4, 2.0, bad),
+    weapon("Holy Water", 5, 1, bad),
+    #---------------Good---------------
+    #---------------Best---------------
+    #---------------Legendary---------------
+]
 
 
 
-#---------------VARIABLES---------------
+
+#---------------STARTER ITEMS---------------
+
+
+
+
 
 
