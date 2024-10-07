aa359535ebe26149d284867615511df5236e2954
Gracie Hutchins <battlesbrown@gmail.com>
2024-10-07 08:57:40 -0500

    Add weapon class, clothes, characters and test

--- logic.py
+++ logic.py
@@ -5,6 +5,10 @@ Logic
 By Ann-MarieDev
 ---------------
 """
+#Test if logic.py is working on main.py
+def test_logic():
+    print("logic.py is working")
+    
 #---------------CLASSES---------------
 
 #Character Class
@@ -17,6 +21,23 @@ class character:
         self.hair_color = hair_color
         self.skin_color = skin_color
         self.eye_color = eye_color
+        
+#Weapon Class
+class weapon:
+    def __init__(self, name, damage, weight):
+        self.name = name
+        self.damage = damage
+        self.weight = weight
+        
+#Clothes/Armor
+class weapon:
+    def __init__(self, name, defense, weight, type, color):
+        self.name = name
+        self.defense = defense
+        self.weight = weight
+        self.type = type
+        self.color = color
+                
 
         
 #---------------CHARACTERS---------------
@@ -31,6 +52,10 @@ main_char_two = character("Orlando", 17, 100, 5, "brown", "tan", "brown")
 enemy_one = character("Alice", 17, 100, 5, "blonde", "pale", "blue")
 
 #--Side Characters/NPC's--
+
+#---------------WEAPONS---------------
+#Starter items
+
 
 
 #---------------VARIABLES---------------
--- main.py
+++ main.py
@@ -5,4 +5,9 @@ Main
 By Ann-MarieDev
 ---------------
 """
+#Import logic.py
+import logic
+
+#Test if logic be wokrin por que no no workie -- it wokris now
+logic.test_logic()
 
