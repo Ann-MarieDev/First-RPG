c682ccd64dd817e6cff705f4cd215239ae16cba8
Gracie Hutchins <battlesbrown@gmail.com>
2024-10-22 18:27:22 -0500

    Add begining story

--- story.py
+++ story.py
@@ -5,7 +5,16 @@ Story -- This holds all the story so it does not take up the entire main.py
 By Ann-MarieDev
 ---------------
 """
+#Imports:
+import time
+
 #---------------Test if story.py is working on main.py---------------
 def test_story():
     print("story.py is working")
+    
+#---------------Story Diag---------------
+
+def beg_story():
+    print("A long time ago...")
+    pass
     
